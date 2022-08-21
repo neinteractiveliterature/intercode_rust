5e2926d9083403c086682ef0dd6047f008abd98c
Nat Budin <nbudin@actbluetech.com>
2022-08-21 15:23:43 -0700

    Trace with Honeycomb

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,22 @@ use async_graphql::*;
 use clap::{command, FromArgMatches, Parser, Subcommand};
 use dotenv::dotenv;
 use intercode_graphql::api;
+use opentelemetry::sdk::trace::{config, Tracer};
+use opentelemetry::sdk::Resource;
+use opentelemetry::KeyValue;
+use opentelemetry_otlp::WithExportConfig;
 use rust_embed::RustEmbed;
 use sea_orm::{ConnectOptions, Database, DatabaseConnection, DbErr};
 use server::serve;
 use std::env;
 use std::time::Duration;
-use tracing::log::*;
+use tonic::metadata::{AsciiMetadataValue, MetadataMap};
+use tonic::transport::ClientTlsConfig;
+use tracing::{log::*, Subscriber};
+use tracing_opentelemetry::OpenTelemetryLayer;
+use tracing_subscriber::layer::Layered;
+use tracing_subscriber::prelude::__tracing_subscriber_SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
 use tracing_subscriber::EnvFilter;
 
 #[cfg(feature = "dhat-heap")]
@@ -53,11 +63,73 @@ async fn connect_database() -> Result<DatabaseConnection, DbErr> {
   Database::connect(connect_options).await
 }
 
+#[cfg(feature = "flamegraph")]
+fn setup_flamegraph_subscriber<
+  S: Subscriber + for<'span> tracing_subscriber::registry::LookupSpan<'span>,
+>(
+  subscriber: S,
+) -> (
+  tracing_subscriber::layer::Layered<
+    tracing_flame::FlameLayer<S, std::io::BufWriter<std::fs::File>>,
+    S,
+    S,
+  >,
+  impl Drop,
+) {
+  eprintln!("Installing flamegraph subscriber; will output to tracing.folded");
+  let (flame_layer, _guard) = tracing_flame::FlameLayer::with_file("./tracing.folded").unwrap();
+
+  (subscriber.with(flame_layer), _guard)
+}
+
+#[cfg(not(feature = "flamegraph"))]
+fn setup_flamegraph_subscriber<S: Subscriber>(subscriber: S) -> (S, impl Drop) {
+  (subscriber, Box::new("shim droppable value"))
+}
+
+fn setup_honeycomb_tracing<
+  S: Subscriber + for<'span> tracing_subscriber::registry::LookupSpan<'span>,
+>(
+  subscriber: S,
+) -> Result<Layered<OpenTelemetryLayer<S, Tracer>, S>> {
+  let mut map = MetadataMap::with_capacity(1);
+  map.insert(
+    "x-honeycomb-team",
+    AsciiMetadataValue::try_from_bytes(env::var("HONEYCOMB_API_KEY")?.as_bytes())?,
+  );
+
+  let tracer = opentelemetry_otlp::new_pipeline()
+    .tracing()
+    .with_exporter(
+      opentelemetry_otlp::new_exporter()
+        .tonic()
+        .with_endpoint(env::var("OTEL_EXPORTER_OTLP_ENDPOINT")?)
+        .with_tls_config(ClientTlsConfig::new())
+        .with_metadata(map),
+    )
+    .with_trace_config(config().with_resource(Resource::new(vec![KeyValue::new(
+      "service.name",
+      env::var("OTEL_SERVICE_NAME")?,
+    )])))
+    .install_batch(opentelemetry::runtime::Tokio)?;
+  let telemetry = tracing_opentelemetry::layer().with_tracer(tracer);
+
+  Ok(subscriber.with(telemetry))
+}
+
 async fn run() -> Result<()> {
-  tracing_subscriber::fmt()
+  let subscriber = tracing_subscriber::fmt()
     .with_env_filter(EnvFilter::from_default_env())
     .with_test_writer()
-    .init();
+    .finish();
+
+  let (subscriber, _guard) = setup_flamegraph_subscriber(subscriber);
+
+  if env::var("OTEL_EXPORTER_OTLP_ENDPOINT").is_ok() {
+    setup_honeycomb_tracing(subscriber)?.init();
+  } else {
+    subscriber.init();
+  }
 
   let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
   info!("Connecting: {}", database_url);
