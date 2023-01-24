e18c1836f0e3260295d4a98951a00fd3d5f7df15
Nat Budin <nbudin@actbluetech.com>
2023-01-24 09:06:51 -0800

    lol I actually need to check in the script

---
+++ graphql-schema-diff.js
@@ -0,0 +1,59 @@
+const { loadSchema } = require("@graphql-tools/load");
+const { UrlLoader } = require("@graphql-tools/url-loader");
+const { GraphQLFileLoader } = require("@graphql-tools/graphql-file-loader");
+const { diff } = require("@graphql-inspector/core");
+
+const intercodeRubySchemaUrl =
+  "https://raw.githubusercontent.com/neinteractiveliterature/intercode/main/schema.graphql";
+
+async function main() {
+  const intercodeRubySchema = await loadSchema(intercodeRubySchemaUrl, {
+    loaders: [new UrlLoader()],
+  });
+
+  const intercodeRustSchema = await loadSchema("./schema.graphql", {
+    loaders: [new GraphQLFileLoader()],
+  });
+
+  const result = await diff(intercodeRubySchema, intercodeRustSchema);
+
+  const missingTypes = result
+    .filter((change) => change.type === "TYPE_REMOVED")
+    .sort((a, b) => a.path.localeCompare(b.path))
+    .map((change) => `- [] ${change.path}`);
+
+  const otherChanges = result
+    .filter((change) => change.type !== "TYPE_REMOVED")
+    .sort((a, b) => {
+      if (
+        a.criticality.level === "BREAKING" &&
+        b.criticality.level != "BREAKING"
+      ) {
+        return -1;
+      }
+
+      if (
+        b.criticality.level === "BREAKING" &&
+        a.criticality.level != "BREAKING"
+      ) {
+        return 1;
+      }
+
+      return a.message.localeCompare(b.message);
+    })
+    .map((change) => `- [] ${change.criticality.level}: ${change.message}`);
+
+  const message = `
+# Missing types
+
+${missingTypes.join("\n")}
+
+# Other changes
+
+${otherChanges.join("\n")}
+  `;
+
+  console.log(message);
+}
+
+main();
